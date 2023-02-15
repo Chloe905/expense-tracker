f886371eaa0d84f6ce9785097910668d3af2150b
Chloe905 <cchloehsieh@gmail.com>
2023-02-15 21:22:04 +0800

    feat:add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -14,4 +14,27 @@ router.post('/', (req, res) => {
     .catch((error) => { console.log(error) })
 })
 
+router.get('/:id/edit', (req, res) => {
+  const { id } = req.params
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
+
+router.post('/:id/edit', (req, res) => {
+  const { id } = req.params
+  const { name, date, category, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
