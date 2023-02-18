ae2197d65621608fdebec0effb8fe3e2cd3b0f5a
Chloe905 <cchloehsieh@gmail.com>
2023-02-18 12:45:45 +0800

    refactor:modify put record route

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -27,15 +27,9 @@ router.get('/:id/edit', (req, res) => {
 router.put('/:id', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
-  const { name, date, category, amount } = req.body
-  return Record.findOne({ _id, userId })
-    .then(record => {
-      record.name = name
-      record.date = date
-      record.category = category
-      record.amount = amount
-      return record.save()
-    })
+  Record.findOneAndUpdate({ _id, userId }, req.body, {
+    new: true
+  })
     .then(() => res.redirect(`/`))
     .catch(error => console.log(error))
 })
